9ea43db4f393a8a2b729c2352fc263a70df485b0
vaabg <150301899+vaabg@users.noreply.github.com>
2023-11-13 23:20:33 +0000

    desafio CSS corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <div className="card">
         <h2>Rick Sanchez</h2>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        <div class="texto-oculto">Texto que aparece ao passar o mouse</div>
       </div>
     </>
   )
