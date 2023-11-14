c0662aae9f0cf0dee889f23f44a1d82ae8d5857d
vaabg <150301899+vaabg@users.noreply.github.com>
2023-11-14 00:10:24 +0000

    Alteração Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
+        <div className="tags">
+          <div classname="tag">Status: Vivo</div>
+          <div classname="tag">Espécie: Humana</div>
+          <div classname="tag">Origem: Terra</div>
+        </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
         <div class="texto-oculto">Texto que aparece ao passar o mouse</div>
       </div>
+      <br />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,3 @@
+export default function card(){
+    return <div>Card está funcionando</div>
+}
